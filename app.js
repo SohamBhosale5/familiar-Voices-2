3068e1a5ef89815b8218d68c72a50117a264eb0d
sohamb5 <sohamb5@uw.edu>
2020-12-06 08:58:32 -0800

    first try

---
+++ app.js
@@ -0,0 +1,35 @@
+
+/*const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://RoseM:FamiliarVoice@familiarvoicecluster.eij5d.mongodb.net/hi?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+  console.log("hi");
+})*/
+
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose')
+
+const url ="mongodb+srv://RoseM:FamiliarVoice@familiarvoicecluster.eij5d.mongodb.net/hi?retryWrites=true&w=majority";
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+    app.use(express.static('public'));
+    const PORT = process.env.PORT || 2000;
+    app.listen(PORT);
